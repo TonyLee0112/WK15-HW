99f1256ac9b66025dfe096b06b2337e0c3e3697c
TonyLee0112 <115335757+TonyLee0112@users.noreply.github.com>
2023-06-09 00:46:35 +0900

    Update Queue.cpp

--- Queue.cpp
+++ Queue.cpp
@@ -1,14 +1,11 @@
 #include <iostream>
 #include <fstream>
-#include <Windows.h>
 #include <vector>
 using namespace std;
 #include "Queue.h"
 
 void Queue::run() {
     for (itime = 0; itime < 120; itime++) {
-        Sleep(0);                           // 1초 동안 대기
-
         ni = rand() % NI;                    // 0부터 NI-1 사이의 랜덤한 값을 생성하여 ni에 저장
         no = rand() % NO;                    // 0부터 NO-1 사이의 랜덤한 값을 생성하여 no에 저장
 
@@ -16,8 +13,8 @@ void Queue::run() {
             lost += q_vec.size() + ni - capacity;   // 패킷 손실 개수 증가
             ni = capacity - q_vec.size();            // 생성되는 패킷 개수 조정하여 용량 초과 방지
         }
-        for (int i = 0; i < ni; i++) {       // ni만큼 랜덤한 값을 queue1 벡터에 추가
-             q_vec.push_back(rand() % NI);
+        for (int i = 0; i < ni; i++) {       // ni만큼 랜덤한 값을 q_vec 벡터에 추가
+            q_vec.push_back(rand() % NI);
         }
 
         for (int i = 0; i < no; i++) {       // no만큼 q_vec에서 패킷을 제거
@@ -25,24 +22,25 @@ void Queue::run() {
                 q_vec.erase(q_vec.begin());
             }
         }
-
-        NofPackets[itime] = (int)q_vec.size();   // 현재 시간에 대한 q_vec의 크기를 배열에 저장
+         NofPackets[itime] = (int)q_vec.size();   // 현재 시간에 대한 q_vec의 크기를 배열에 저장
     }
 }
-float Queue::AverageP(int *a) {
+float Queue::AverageP() {
     float sum = 0;
-    for (int i = 0; i < 120; i++) {
-        sum += a[i];
-    };
-    return sum / 120;
-    
+
+    for (int i = 0; i < capacity; i++) {
+        sum += *(NofPackets + i);  // 배열 a, b, c에서 해당 시간에 대한 패킷 개수를 더함
+    }
+
+    return sum / capacity;                     // 총 패킷 개수를 시간의 3배로 나누어 평균값을 구함
 }
-float Queue::maxValue(int* a) {
+float Queue::maxValue() {
     int max = 0;
-    for (int i = 0; i < 120; i++) {
-        if (a[i] > max) {                      // 배열 a에서 최댓값을 찾음
-            max = a[i];
+    for (int i = 0; i < capacity; i++) {
+        if (NofPackets[i] > max) {                      // 배열 a에서 최댓값을 찾음
+            max = NofPackets[i];
         }
     }
+
     return max;                               // 최댓값 반환
 }
